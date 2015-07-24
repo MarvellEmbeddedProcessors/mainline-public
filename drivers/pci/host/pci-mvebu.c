4ca92b7c6eef5f48ef81a24432bc4810e9e3c8c7
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2015-07-24 15:18:55 +0200

    pci: pci-mvebu: add support for SERDES phy
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- drivers/pci/host/pci-mvebu.c
+++ drivers/pci/host/pci-mvebu.c
@@ -15,21 +15,51 @@
 #include <linux/mbus.h>
 #include <linux/msi.h>
 #include <linux/slab.h>
+#include <linux/phy/phy.h>
 #include <linux/platform_device.h>
 #include <linux/of_address.h>
 #include <linux/of_irq.h>
 #include <linux/of_gpio.h>
 #include <linux/of_pci.h>
 #include <linux/of_platform.h>
+#include <linux/mfd/syscon.h>
+#include <linux/mfd/syscon/mvebu-system-controller.h>
+#include <linux/regmap.h>
 
 /*
  * PCIe unit register offsets.
  */
 #define PCIE_DEV_ID_OFF		0x0000
+#define   PCIE_DEV_ID_SHIFT		16
+#define   PCIE_DEV_ID_MASK		(0xffff << PCIE_DEV_ID_SHIFT)
 #define PCIE_CMD_OFF		0x0004
 #define PCIE_DEV_REV_OFF	0x0008
 #define PCIE_BAR_LO_OFF(n)	(0x0010 + ((n) << 3))
 #define PCIE_BAR_HI_OFF(n)	(0x0014 + ((n) << 3))
+#define PCIE_CAPABILITIES_OFF	0x0060
+#define   PCIE_CAP_DEVTYPE_MASK	(0xF << 20)
+#define	  PCIE_CAP_DEVTYPE_RC	(0x4 << 20)
+#define   PCIE_CAP_DEVTYPE_EP	(0x1 << 20)
+#define PCIE_LINK_CAPS_OFF		0x006c
+#define   PCIE_LINK_CAP_SPD_MASK	0xf
+#define   PCIE_LINK_CAP_SPD_2_5G	0x1
+#define   PCIE_LINK_CAP_SPD_5G		0x2
+#define   PCIE_LINK_CAP_WIDTH_SHIFT	4
+#define   PCIE_LINK_CAP_WIDTH_MASK	0x3f0
+#define	  PCIE_LINK_CAP_WIDTH_1		(1 << PCIE_LINK_CAP_WIDTH_SHIFT)
+#define   PCIE_LINK_CAP_WIDTH_4		(4 << PCIE_LINK_CAP_WIDTH_SHIFT)
+#define PCIE_LINK_CTRL_STAT_OFF		0x0070
+#define   PCIE_LINK_CTRL_COMMON_CLK	BIT(6)
+#define   PCIE_LINK_CTRL_SPD_SHIFT	16
+#define   PCIE_LINK_CTRL_SPD_MASK	(0xf << PCIE_LINK_CTRL_SPD_SHIFT)
+#define   PCIE_LINK_CTRL_SPD_2_5G	(0x1 << PCIE_LINK_CTRL_SPD_SHIFT)
+#define   PCIE_LINK_CTRL_SPD_5G		(0x2 << PCIE_LINK_CTRL_SPD_SHIFT)
+#define   PCIE_LINK_CTRL_WIDTH_SHIFT	20
+#define   PCIE_LINK_CTRL_WIDTH_MASK	(0x3f << PCIE_LINK_CTRL_WIDTH_SHIFT)
+#define PCIE_LINK_CTRL_STAT2_OFF	0x0090
+#define   PCIE_LINK_CTRL2_SPD_MASK	0xf
+#define   PCIE_LINK_CTRL2_SPD_2_5G	0x1
+#define   PCIE_LINK_CTRL2_SPD_5G	0x2
 #define PCIE_HEADER_LOG_4_OFF	0x0128
 #define PCIE_BAR_CTRL_OFF(n)	(0x1804 + (((n) - 1) * 4))
 #define PCIE_WIN04_CTRL_OFF(n)	(0x1820 + ((n) << 4))
@@ -53,12 +83,27 @@
 #define  PCIE_MASK_ENABLE_INTS          0x0f000000
 #define PCIE_CTRL_OFF		0x1a00
 #define  PCIE_CTRL_X1_MODE		0x0001
+#define  PCIE_CTRL_CONF_AUTOSPEED	BIT(10)
 #define PCIE_STAT_OFF		0x1a04
 #define  PCIE_STAT_BUS                  0xff00
 #define  PCIE_STAT_DEV                  0x1f0000
 #define  PCIE_STAT_LINK_DOWN		BIT(0)
 #define PCIE_DEBUG_CTRL         0x1a60
 #define  PCIE_DEBUG_SOFT_RESET		BIT(20)
+#define PCIE_DEBUG_STATUS_OFF	0x1a64
+#define PCIE_PHY_ACCESS_OFF	0x1b00
+#define  PCIE_PHY_DATA_SHIFT		0
+#define  PCIE_PHY_OFFSET_SHIFT		16
+#define  PCIE_PHY_LANE_SHIFT		24
+#define  PCIE_PHY_RD_ACCESS		BIT(31)
+
+/*
+ * PCIe PHY registers, accessed from the indirect PCIE_PHY_ACCESS_OFF
+ * register.
+ */
+#define GLOB_CLK_CTRL		0xc1
+#define GLOB_TEST_CTRL		0xc2
+#define GLOB_CLK_SRC_LO		0xc3
 
 /* PCI configuration space of a PCI-to-PCI bridge */
 struct mvebu_sw_pci_bridge {
@@ -93,6 +138,16 @@ struct mvebu_sw_pci_bridge {
 	u16 bridgectrl;
 };
 
+enum mvebu_pcie_variant {
+	PCIE_VARIANT_AXP = 0x1,
+	PCIE_VARIANT_A370,
+	PCIE_VARIANT_A375,
+	PCIE_VARIANT_A38X,
+	PCIE_VARIANT_A39X,
+	PCIE_VARIANT_KIRKWOOD,
+	PCIE_VARIANT_DOVE,
+};
+
 struct mvebu_pcie_port;
 
 /* Structure representing all PCIe interfaces */
@@ -100,10 +155,12 @@ struct mvebu_pcie {
 	struct platform_device *pdev;
 	struct mvebu_pcie_port *ports;
 	struct msi_controller *msi;
+	struct regmap *syscon_regmap;
 	struct resource io;
 	struct resource realio;
 	struct resource mem;
 	struct resource busn;
+	enum mvebu_pcie_variant variant;
 	int nports;
 };
 
@@ -113,6 +170,7 @@ struct mvebu_pcie_port {
 	void __iomem *base;
 	u32 port;
 	u32 lane;
+	int nlanes;
 	int devfn;
 	unsigned int mem_target;
 	unsigned int mem_attr;
@@ -140,6 +198,28 @@ static inline void mvebu_writel(struct mvebu_pcie_port *port, u32 val, u32 reg)
 static inline u32 mvebu_readl(struct mvebu_pcie_port *port, u32 reg)
 {
 	return readl(port->base + reg);
+}
+
+static void mvebu_phy_write(struct mvebu_pcie_port *port, int lane, int offset, u16 data)
+{
+	u32 value;
+
+	value = (lane << PCIE_PHY_LANE_SHIFT) |
+		(offset << PCIE_PHY_OFFSET_SHIFT) |
+		(data << PCIE_PHY_DATA_SHIFT);
+	mvebu_writel(port, value, PCIE_PHY_ACCESS_OFF);
+}
+
+static u16 mvebu_phy_read(struct mvebu_pcie_port *port, int lane, int offset)
+{
+	u32 value;
+
+	value = PCIE_PHY_RD_ACCESS |
+		(lane << PCIE_PHY_LANE_SHIFT) |
+		(offset << PCIE_PHY_OFFSET_SHIFT);
+	mvebu_writel(port, value, PCIE_PHY_ACCESS_OFF);
+
+	return mvebu_readl(port, PCIE_PHY_ACCESS_OFF) & 0xFFFF;
 }
 
 static inline bool mvebu_has_ioport(struct mvebu_pcie_port *port)
@@ -781,6 +861,407 @@ static resource_size_t mvebu_pcie_align_resource(struct pci_dev *dev,
 		return start;
 }
 
+static void mvebu_pcie_disable_all_ifaces(struct mvebu_pcie *pcie)
+{
+	u32 mask;
+
+	/*
+	 * Disable interfaces, and clear PCIe clkout bits and x4
+	 * configuration
+	 */
+	switch(pcie->variant)
+	{
+	case PCIE_VARIANT_AXP:
+		mask = BIT(8) | BIT(7) | BIT(5) | BIT(4) | 0xF;
+		break;
+	case PCIE_VARIANT_A370:
+		mask = 0x3;
+		break;
+	case PCIE_VARIANT_A375:
+		mask = BIT(5) | BIT(4) | 0x3;
+		break;
+	case PCIE_VARIANT_A38X:
+	case PCIE_VARIANT_A39X:
+		mask = BIT(15) | BIT(14) | BIT(5) | BIT(4) | 0xF;
+		break;
+	/*
+	 * We don't support low-level PCIe init with Kirkwood and
+	 * Dove
+	 */
+	default:
+		return;
+	}
+
+	regmap_update_bits(pcie->syscon_regmap, SOC_CONTROL_REG, mask, 0);
+
+	/* Clear the PCIe related bits in COMPHY_REF_CLK_ALIGN */
+	if (pcie->variant == PCIE_VARIANT_AXP ||
+	    pcie->variant == PCIE_VARIANT_A370)
+		regmap_update_bits(pcie->syscon_regmap, COMPHY_REF_CLK_ALIGN,
+				   COMPHY_REF_CLK_ALIGN_PCIE_MASK, 0);
+}
+
+static void mvebu_pcie_enable_iface(struct mvebu_pcie_port *port,
+				    bool enable)
+{
+	u32 mask = BIT(port->port);
+
+	regmap_update_bits(port->pcie->syscon_regmap,
+			   SOC_CONTROL_REG, mask,
+			   enable ? mask : 0);
+}
+
+/*
+ * When enable is true, sets the interface in "quad" mode, i.e 4
+ * independent x1 interfaces
+ */
+static int mvebu_pcie_set_quad_iface(struct mvebu_pcie_port *port,
+				     bool enable)
+{
+	u32 mask;
+
+	/* Make sure the PCIe interface is x1 */
+	if (port->nlanes != 1)
+		return -EINVAL;
+
+	switch(port->pcie->variant)
+	{
+	case PCIE_VARIANT_AXP:
+		/*
+		 * On Armada XP, bits 7 and 8 control x1/x4 configuration
+		 * for PCIe0 and PCIe1 respectively
+		 */
+		mask = BIT(port->port + 7);
+		regmap_update_bits(port->pcie->syscon_regmap,
+				   SOC_CONTROL_REG, mask,
+				   enable ? mask : 0);
+		break;
+	case PCIE_VARIANT_A38X:
+	case PCIE_VARIANT_A39X:
+		/*
+		 * On Armada 38x/39x, bit 14 controls x4 configuration
+		 * for PCIe0. Other PCIe interfaces are not x4
+		 * compatible.
+		 */
+		mask = BIT(14);
+		regmap_update_bits(port->pcie->syscon_regmap,
+				   SOC_CONTROL_REG, mask,
+				   enable ? mask : 0);
+		break;
+	default:
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+static int mvebu_pcie_set_clkout(struct mvebu_pcie_port *port,
+				 bool enable)
+{
+	u32 mask;
+
+	/* No such configuration on Armada 370 */
+	if (port->pcie->variant == PCIE_VARIANT_A370)
+		return -EINVAL;
+
+	/* Only available for the first two interfaces */
+	if (port->port > 1)
+		return -EINVAL;
+
+	/* Bits 4 and 5 controls clkout for PCIe0 and PCIe1 */
+	mask = BIT(port->port + 4);
+	regmap_update_bits(port->pcie->syscon_regmap, SOC_CONTROL_REG,
+			   mask, enable ? mask : 0);
+
+	return 0;
+}
+
+
+/*
+ * Power on all PHYs needed for a given PCIe port, which will in fact
+ * set up the SERDES lanes or COMPHY depending on the SoC.
+ */
+static void mvebu_pcie_phy_power_on(struct mvebu_pcie_port *port)
+{
+	struct device *dev = &port->pcie->pdev->dev;
+	int phyi;
+
+	for (phyi = 0; phyi < port->nlanes; phyi++) {
+		struct phy *phy;
+
+		phy = devm_of_phy_get_by_index(dev, port->dn, phyi);
+		if (IS_ERR(phy)) {
+			dev_err(dev, "PCIe%d.%d: cannot get PHY %d: %ld\n",
+				port->port, port->lane, phyi, PTR_ERR(phy));
+			continue;
+		} else {
+			int ret;
+
+			ret = phy_power_on(phy);
+			if (ret) {
+				dev_err(dev, "PCIe%d.%d: cannot power-up PHY %d\n",
+					port->port, port->lane, phyi);
+				continue;
+			}
+		}
+	}
+}
+
+/*
+ * This function does the initialization of the PHY and SERDES, for
+ * Armada 370 and Armada XP
+ */
+static void mvebu_pcie_serdes_phy_init(struct mvebu_pcie_port *port)
+{
+	u32 rx_impedance_mode, reg;
+	u16 model = 0;
+
+	/* Apply soft reset + fixed pclk mode */
+	mvebu_phy_write(port, 0, GLOB_CLK_CTRL, 0x25);
+
+	/* Multicast enable bit configuration */
+	if (port->nlanes == 4)
+		mvebu_phy_write(port, 0, GLOB_TEST_CTRL, 0x200);
+
+	/* Clock control */
+	if (port->nlanes == 1)
+		mvebu_phy_write(port, 0, GLOB_CLK_SRC_LO, 0x0f);
+
+	mvebu_phy_write(port, 0, 0xC5, 0x11f);
+
+	/*
+	 * Power UP the PHY here to get the proper muxing of the
+	 * SERDES lanes.
+	 */
+	mvebu_pcie_phy_power_on(port);
+
+	/*
+	 * Activate the RX High Impedance Mode field (bit [2]) in
+	 * register PCIe_USB Control. Set bit[12]: The analog part
+	 * latches idle if PU_TX = 1 and PU_PLL = 1.
+	 */
+	rx_impedance_mode = 0x8080;
+	if (port->pcie->variant == PCIE_VARIANT_AXP)
+		rx_impedance_mode |= 0x4;
+
+#if 0
+	/* WHYY ? With this, value = 0x1084 instead of 0x9084 */
+	if (port->nlanes == 4)
+		rx_impedance_mode &= 0xff;
+#endif
+
+	mvebu_phy_write(port, port->lane, 0x48, 0x1000 | rx_impedance_mode);
+
+	/* Configure x4 or x1 for the first two ports on Armada XP and clkout */
+
+	if (port->pcie->variant == PCIE_VARIANT_AXP &&
+	    port->port <= 1 && port->lane == 0) {
+		u32 reg;
+
+		mvebu_pcie_set_quad_iface(port, port->nlanes == 1);
+
+		regmap_read(port->pcie->syscon_regmap, SAMPLE_AT_RESET, &reg);
+		mvebu_pcie_set_clkout(port, reg & SAR_PCIE_CLKOUT);
+	}
+
+
+	/* PCI Express Link Capabilities */
+
+	reg = mvebu_readl(port, PCIE_LINK_CTRL_STAT_OFF);
+	reg |= PCIE_LINK_CTRL_COMMON_CLK;
+	mvebu_writel(port, reg, PCIE_LINK_CTRL_STAT_OFF);
+
+	reg = mvebu_readl(port, PCIE_LINK_CAPS_OFF);
+	reg &= ~(PCIE_LINK_CAP_SPD_MASK | PCIE_LINK_CAP_WIDTH_MASK);
+#if 1
+	if (port->nlanes == 4)
+		reg |= PCIE_LINK_CAP_WIDTH_4;
+	else
+		reg |= PCIE_LINK_CAP_WIDTH_1;
+#else
+	reg |= PCIE_LINK_CAP_WIDTH_1;
+#endif
+
+	/* Assume we support GEN2. FIXME: Check with Marvell. */
+	reg |= PCIE_LINK_CAP_SPD_5G;
+	mvebu_writel(port, reg, PCIE_LINK_CAPS_OFF);
+
+	/* Configure the Reference Clock Align register for the x4 ports */
+	if (port->nlanes == 4)
+		regmap_update_bits(port->pcie->syscon_regmap, COMPHY_REF_CLK_ALIGN,
+				   0xF << port->port, 0xF << port->port);
+
+	/* Prepare PHY parameters */
+	if ((port->nlanes == 4 && port->port == 0) ||
+	    port->nlanes == 1) {
+		/* Set operational mode and reference clock */
+		mvebu_phy_write(port, port->lane, 0x1, 0xFC60);
+
+		if (port->pcie->variant == PCIE_VARIANT_AXP) {
+			u16 val;
+
+			/* Configure max PLL rate (bit 8) in KVCO Calibration Control */
+			val = mvebu_phy_read(port, port->lane, 0x2);
+			/* TODO: WEIRD. The U-Boot code sets bit 8, but in practice it doesn't seem to be set */
+///			val |= BIT(8);
+			mvebu_phy_write(port, port->lane, 0x2, val);
+
+			/* Use Maximum PLL Rate (bits [10:9]) */
+			val = mvebu_phy_read(port, port->lane, 0x81);
+			val |= BIT(10) | BIT(9);
+			mvebu_phy_write(port, port->lane, 0x81, val);
+		}
+	}
+
+	/* Last phase of PEX-PIPE Configuration */
+	mvebu_phy_write(port, port->lane, GLOB_CLK_CTRL, 0x24);
+
+	/* Make sure we're working in Root Complex mode */
+	reg = mvebu_readl(port, PCIE_CAPABILITIES_OFF);
+	reg &= ~PCIE_CAP_DEVTYPE_MASK;
+	reg |= PCIE_CAP_DEVTYPE_RC;
+	mvebu_writel(port, reg, PCIE_CAPABILITIES_OFF);
+
+	/*
+	 * Change of Slew Rate port0. U-Boot source code indicates
+	 * 0x2a21, but dumping the register shows 0xaa21.
+	 */
+	mvebu_phy_write(port, port->lane, 0xF, 0xaa21);
+
+	/*
+	 * Change PLL BW port0. U-Boot source code indicates 0x6219,
+	 * but dumping the register shows 0x6205.
+	 */
+	mvebu_phy_write(port, port->lane, 0x4F, 0x6205);
+
+	mvebu_pcie_enable_iface(port, true);
+
+	/* Update the PCIe device ID */
+	if (port->pcie->variant == PCIE_VARIANT_A370)
+		model = 0x6710;
+	else {
+		if (of_machine_is_compatible("marvell,armadaxp-mv78460"))
+			model = 0x7846;
+		else if (of_machine_is_compatible("marvell,armadaxp-mv78260"))
+			model = 0x7826;
+		else if (of_machine_is_compatible("marvell,armadaxp-mv78230"))
+			model = 0x7823;
+	}
+
+	reg = mvebu_readl(port, PCIE_DEV_ID_OFF);
+	reg &= ~PCIE_DEV_ID_MASK;
+	reg |= (model << PCIE_DEV_ID_SHIFT);
+	mvebu_writel(port, reg, PCIE_DEV_ID_OFF);
+
+#if 0
+	pr_info(" === Register dump\n");
+	pr_info("  PHY @ 0, 0xC1: 0x%x\n", mvebu_phy_read(port, 0, 0xC1));
+	pr_info("  PHY @ 0, 0xC2: 0x%x\n", mvebu_phy_read(port, 0, 0xC2));
+	pr_info("  PHY @ 0, 0xC3: 0x%x\n", mvebu_phy_read(port, 0, 0xC3));
+	pr_info("  PHY @ 0, 0xC3: 0x%x\n", mvebu_phy_read(port, 0, 0xC5));
+	pr_info("  PHY @ lane, 0x48: 0x%x\n", mvebu_phy_read(port, port->lane, 0x48));
+	regmap_read(port->pcie->syscon_regmap, SOC_CONTROL_REG, &reg);
+	pr_info("  SOC_CONTROL_REG: 0x%x\n", reg);
+	pr_info("  PCIE_LINK_CTRL_STAT_OFF: 0x%x\n", mvebu_readl(port, PCIE_LINK_CTRL_STAT_OFF));
+	pr_info("  PCIE_LINK_CAPS_OFF: 0x%x\n", mvebu_readl(port, PCIE_LINK_CAPS_OFF));
+	pr_info("  PHY @ lane, 0x2: 0x%x\n", mvebu_phy_read(port, port->lane, 0x2));
+	pr_info("  PHY @ lane, 0x81: 0x%x\n", mvebu_phy_read(port, port->lane, 0x81));
+	pr_info("  PHY @ lane, 0xC1: 0x%x\n", mvebu_phy_read(port, port->lane, 0xC1));
+	pr_info("  PCIE_CAPABILITIES_OFF: 0x%x\n", mvebu_readl(port, PCIE_CAPABILITIES_OFF));
+	pr_info("  PHY @ lane, 0xF: 0x%x\n", mvebu_phy_read(port, port->lane, 0xF));
+	pr_info("  PHY @ lane, 0x4F: 0x%x\n", mvebu_phy_read(port, port->lane, 0x4F));
+	pr_info("  PCIE_DEV_ID_OFF: 0x%x\n", mvebu_readl(port, PCIE_DEV_ID_OFF));
+#endif
+}
+
+/*
+ * This function does the initialization of the COMPHY for Armada 375,
+ * 38x and 39x.
+ */
+static void mvebu_pcie_comphy_init(struct mvebu_pcie_port *port)
+{
+	/*
+	 * Nothing else to be done, since the PHY initialization is
+	 * actually done by the COMPHY driver.
+	 */
+	mvebu_pcie_phy_power_on(port);
+}
+
+static void mvebu_pcie_adjust_speed(struct pci_dev *dev)
+{
+	u32 reg;
+	u16 pos, word;
+	struct mvebu_pcie_port *port;
+	struct mvebu_pcie *pcie;
+	int width;
+
+	/*
+	 * We care only about the real devices, not the root bus that
+	 * has the emulated PCI-to-PCI bridges.
+	 */
+	if (dev->bus->number == 0)
+		return;
+
+	pcie = sys_to_pcie(dev->bus->parent->sysdata);
+
+	port = mvebu_pcie_find_port(pcie, dev->bus, dev->devfn);
+	if (!port)
+		return;
+
+	reg = mvebu_readl(port, PCIE_DEBUG_STATUS_OFF);
+
+	/* Undocumented magic values */
+	if ((reg & 0x7f) != 0x7e)
+		return;
+
+	/*
+	 * Query the actual negotiated link width, and if smaller than
+	 * the width of the interface, reduce it.
+	 */
+	reg = mvebu_readl(port, PCIE_LINK_CTRL_STAT_OFF);
+	width = (reg & PCIE_LINK_CTRL_WIDTH_MASK) >>
+		PCIE_LINK_CTRL_WIDTH_SHIFT;
+	if (width < port->nlanes) {
+		reg = mvebu_readl(port, PCIE_LINK_CAPS_OFF);
+		reg &= ~PCIE_LINK_CAP_WIDTH_MASK;
+		reg |= width << PCIE_LINK_CAP_WIDTH_SHIFT;
+		mvebu_writel(port, reg, PCIE_LINK_CAPS_OFF);
+	}
+
+	/*
+	 * If we're capable of doing GEN2, but the link is currently
+	 * configured as GEN1, then let's query the EP to see if it is
+	 * GEN2 capable.
+	 */
+
+	/* Are we capable of doing GEN2 (5G) ? If not, bail out */
+	reg = mvebu_readl(port, PCIE_LINK_CAPS_OFF);
+	if ((reg & PCIE_LINK_CAP_SPD_MASK) != PCIE_LINK_CAP_SPD_5G)
+		return;
+
+	/* Is the link already GEN2 ? If yes, bail out */
+	reg = mvebu_readl(port, PCIE_LINK_CTRL_STAT_OFF);
+	if ((reg & PCIE_LINK_CTRL_SPD_MASK) != PCIE_LINK_CTRL_SPD_2_5G)
+		return;
+
+	pos = pci_bus_find_capability(dev->bus, dev->devfn, PCI_CAP_ID_EXP);
+	pci_bus_read_config_word(dev->bus, dev->devfn, pos + PCI_EXP_LNKSTA, &word);
+
+	/* If the device actually supports GEN2, upgrade the link speed */
+	if ((word & PCI_EXP_LNKSTA_CLS) >= PCI_EXP_LNKSTA_CLS_5_0GB) {
+		reg = mvebu_readl(port, PCIE_LINK_CTRL_STAT2_OFF);
+		reg &= ~PCIE_LINK_CTRL2_SPD_MASK;
+		reg |= PCIE_LINK_CTRL2_SPD_5G;
+		mvebu_writel(port, reg, PCIE_LINK_CTRL_STAT2_OFF);
+
+		reg = mvebu_readl(port, PCIE_CTRL_OFF);
+		reg |= PCIE_CTRL_CONF_AUTOSPEED;
+		mvebu_writel(port, reg, PCIE_CTRL_OFF);
+	}
+}
+
+DECLARE_PCI_FIXUP_FINAL(PCI_ANY_ID, PCI_ANY_ID, mvebu_pcie_adjust_speed);
+
 static void mvebu_pcie_enable(struct mvebu_pcie *pcie)
 {
 	struct hw_pci hw;
@@ -914,11 +1395,24 @@ static int mvebu_pcie_resume(struct device *dev)
 	return 0;
 }
 
+static const struct of_device_id mvebu_pcie_of_match_table[] = {
+	{ .compatible = "marvell,armada-xp-pcie",  .data = (void *) PCIE_VARIANT_AXP },
+	{ .compatible = "marvell,armada-370-pcie", .data = (void *) PCIE_VARIANT_A370 },
+	{ .compatible = "marvell,armada-375-pcie", .data = (void *) PCIE_VARIANT_A375 },
+	{ .compatible = "marvell,armada-38x-pcie", .data = (void *) PCIE_VARIANT_A38X },
+	{ .compatible = "marvell,armada-39x-pcie", .data = (void *) PCIE_VARIANT_A39X },
+	{ .compatible = "marvell,dove-pcie",       .data = (void *) PCIE_VARIANT_DOVE },
+	{ .compatible = "marvell,kirkwood-pcie",   .data = (void *) PCIE_VARIANT_KIRKWOOD },
+	{},
+};
+MODULE_DEVICE_TABLE(of, mvebu_pcie_of_match_table);
+
 static int mvebu_pcie_probe(struct platform_device *pdev)
 {
 	struct mvebu_pcie *pcie;
 	struct device_node *np = pdev->dev.of_node;
 	struct device_node *child;
+	const struct of_device_id *of_dev;
 	int i, ret;
 
 	pcie = devm_kzalloc(&pdev->dev, sizeof(struct mvebu_pcie),
@@ -928,6 +1422,9 @@ static int mvebu_pcie_probe(struct platform_device *pdev)
 
 	pcie->pdev = pdev;
 	platform_set_drvdata(pdev, pcie);
+
+	of_dev = of_match_device(mvebu_pcie_of_match_table, &pdev->dev);
+	pcie->variant = (int) of_dev->data;
 
 	/* Get the PCIe memory and I/O aperture */
 	mvebu_mbus_get_pcie_mem_aperture(&pcie->mem);
@@ -954,6 +1451,16 @@ static int mvebu_pcie_probe(struct platform_device *pdev)
 			ret);
 		return ret;
 	}
+
+	/*
+	 * We intentionally don't check the return value, since we
+	 * want, for DT backward compatibility reason, to continue
+	 * supporting platforms that don't have the syscon phandle.
+	 */
+	pcie->syscon_regmap =
+		syscon_regmap_lookup_by_phandle(np, "marvell,syscon");
+	if (!IS_ERR(pcie->syscon_regmap))
+		mvebu_pcie_disable_all_ifaces(pcie);
 
 	i = 0;
 	for_each_child_of_node(pdev->dev.of_node, child) {
@@ -991,6 +1498,9 @@ static int mvebu_pcie_probe(struct platform_device *pdev)
 
 		port->name = kasprintf(GFP_KERNEL, "pcie%d.%d",
 				       port->port, port->lane);
+
+		if (of_property_read_u32(child, "marvell,nlanes", &port->nlanes))
+			port->nlanes = 1;
 
 		port->devfn = of_pci_get_devfn(child);
 		if (port->devfn < 0)
@@ -1057,8 +1567,14 @@ static int mvebu_pcie_probe(struct platform_device *pdev)
 		}
 
 		mvebu_pcie_set_local_dev_nr(port, 1);
-
 		port->dn = child;
+
+		if (pcie->variant == PCIE_VARIANT_A370 ||
+		    pcie->variant == PCIE_VARIANT_AXP)
+			mvebu_pcie_serdes_phy_init(port);
+		else
+			mvebu_pcie_comphy_init(port);
+
 		mvebu_sw_pci_bridge_init(port);
 		i++;
 	}
@@ -1075,15 +1591,6 @@ static int mvebu_pcie_probe(struct platform_device *pdev)
 
 	return 0;
 }
-
-static const struct of_device_id mvebu_pcie_of_match_table[] = {
-	{ .compatible = "marvell,armada-xp-pcie", },
-	{ .compatible = "marvell,armada-370-pcie", },
-	{ .compatible = "marvell,dove-pcie", },
-	{ .compatible = "marvell,kirkwood-pcie", },
-	{},
-};
-MODULE_DEVICE_TABLE(of, mvebu_pcie_of_match_table);
 
 static struct dev_pm_ops mvebu_pcie_pm_ops = {
 	.suspend_noirq = mvebu_pcie_suspend,
