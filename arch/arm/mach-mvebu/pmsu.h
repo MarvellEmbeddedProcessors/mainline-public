d636a7e3d1052764d6a829af65cd1e383975de0d
Gregory CLEMENT <gregory.clement@free-electrons.com>
2015-10-01 11:31:45 +0200

    Revert "ARM: mvebu: Handle the CPU freq case for Armada 38x suspend"
    This reverts commit 21213a5be3969e1c80b37edbb1279be8d3fb911f.

--- arch/arm/mach-mvebu/pmsu.h
+++ arch/arm/mach-mvebu/pmsu.h
@@ -21,5 +21,4 @@ void armada_370_xp_cpu_resume(void);
 
 int armada_370_xp_pmsu_idle_enter(unsigned long deepidle);
 int armada_38x_do_cpu_suspend(unsigned long deepidle);
-void mvebu_v7_pmsu_disable_dfs_cpu(int hw_cpu);
 #endif	/* __MACH_370_XP_PMSU_H */
